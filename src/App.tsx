a63257676b05f48e343c33891512099ff0c19d99
Ronaldo <ronaldosantana483@gmail.com>
2021-08-04 20:12:27 -0300

    Configurações iniciais de fontes e cores.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "./styles/global";
+export function App() {
+
+  return (
+    <div className="app">
+        <h1>Hellow men</h1>
+        <GlobalStyle />
+
+    </div>
+  
+  );
+}
+
